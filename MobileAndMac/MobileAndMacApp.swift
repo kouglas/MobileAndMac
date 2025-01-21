2d3884fdefb5d8a1a06f71a83f0273b36cc9c887
Kari <kouglas2@gmail.com>
2025-01-21 15:18:52 -0600

    Initial Commit

---
+++ MobileAndMac/MobileAndMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileAndMacApp.swift
+//  MobileAndMac
+//
+//  Created by Kari Douglas on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct MobileAndMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
